d1cc26f02b6ae57891c7b8bdada8e90e5f9f1b29
Darsh Balani <darshbalani@berkeley.edu>
2024-06-10 20:05:06 -0700

    dummy changes

--- deoldify/dataset.py
+++ deoldify/dataset.py
@@ -40,5 +40,5 @@ def get_colorize_data(
 def get_dummy_databunch() -> ImageDataBunch:
     path = Path('./dummy/')
     return get_colorize_data(
-        sz=1, bs=1, crappy_path=path, good_path=path, keep_pct=0.001
+        sz=1, bs=1, bw_path=path, color_path=path, keep_pct=0.001
     )
